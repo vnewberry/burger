4ce5edd16e7ec44c99bee59f9ed752f396f28f34
Vance Newberry <vancenewberry@gmail.com>
2020-05-11 15:30:24 -0500

    added sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Slider');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');

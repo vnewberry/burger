e6bcf59f8cf9e721dab11f07e8227e365dc6f1f0
Vance Newberry <vancenewberry@gmail.com>
2020-05-15 21:31:22 -0500

    fixed css

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 var path = require("path");
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname+"public"));
+app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join('public')));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
